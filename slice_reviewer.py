0102452998944cede5fedeea327769b3a2ae1cf8
bovardj <bovardj@gmail.com>
2024-01-08 12:10:52 -0700

    (was supposed to be in prev commit) Created block comments (triple quotes) to clarify code organization. Made a comment on potentially improving plotting loop

--- slice_reviewer.py
+++ slice_reviewer.py
@@ -37,6 +37,9 @@ def main():
     # except FileNotFoundError:
     #     print('no data file found, please put the vessel_measure.json in the ./data folder')
 
+    '''
+    Read in the data
+    '''
     try:
         data_loc = pd.read_csv(get_file_path('data/loc.csv'))
         sid_filter = pd.read_csv(get_file_path('data/clin_data_selected.csv'))
@@ -50,7 +53,10 @@ def main():
     except FileNotFoundError:
         print('No data file found, please put the vessel_measure.json in the ./data folder.')
 
-    # build the GUI
+    '''
+    Build the GUI
+    '''
+    # The root window is the main GUI window
     root = tk.Tk()
     root.title("Slice based Data Visualization")
 
@@ -80,6 +86,9 @@ def main():
     # build the multi-select checkbox
     checkbox_var = {}
 
+    '''
+    Update the filtered patients when the checkboxes are clicked
+    '''
     def update_filtered_patients():
         filtered_data = sid_filter.copy()
         for key, var in checkbox_var.items():
@@ -99,6 +108,9 @@ def main():
         # num_patients_label.config(text = f"Number of filtered patients: \t {num_filtered_patients} of {num_total_patients} total patients")
         num_patients_label_count.config(text = f"{num_filtered_patients} of {num_total_patients} total patients")
 
+    '''
+    Create the portion of the GUI for risk factor filters: ['Risk', 'Smoker', 'Diabetes', 'Stroke', 'Heart Attack']
+    '''
     # 创建并放置复选框
     # Create the checkboxes for ['Risk', 'Smoker', 'Diabetes', 'Stroke', 'Heart Attack']
     for i, value in enumerate(filter_sid):
@@ -109,6 +121,9 @@ def main():
         else:
             checkbox.grid(row=3, column=i, sticky='w', padx = (0, 12))
 
+    '''
+    Create the portion of the GUI for the patient ID selection
+    '''
     # Add a label for the patient id combobox
     patient_id_dropdown_label = ttk.Label(top_left_frame, text=f"Select a patient ID:")
     patient_id_dropdown_label.grid(row=1, column=0, padx=5, pady=5, sticky='e')
@@ -121,20 +136,20 @@ def main():
     patient_id_dropdown.grid(row=1, column=1, padx=5, pady=5)
 
     # Get the number of filtered patients. 
-    # num_filtered_patients = len(patient_id_dropdown['values'])
-    # print(f'number of total patients: {num_filtered_patients}')
     num_total_patients = len(patient_id_dropdown['values'])
     print(f'number of total patients: {num_total_patients}')
     # Nothing is filtered yet, so this is the total number of patients
     num_filtered_patients = num_total_patients
 
     # Add a label that displays the number of patient ids that match the filter
-    # num_patients_label = ttk.Label(top_left_frame, text=f"Number of filtered patients: \t {num_filtered_patients} of {num_total_patients} total patients")
     num_patients_label = ttk.Label(top_left_frame, text=f"Number of filtered patients:")
     num_patients_label.grid(row=0, column=0, padx=5, pady=(10, 5), sticky='w')
     num_patients_label_count = ttk.Label(top_left_frame, text=f"{num_filtered_patients} of {num_total_patients} total patients")
     num_patients_label_count.grid(row=0, column=1, padx=5, pady=(10, 5), sticky='e')
 
+    '''
+    Create the portion of the GUI for the measurement selection
+    '''
     # Add a label for the measurement type combobox
     which_measure_dropdown_label = ttk.Label(top_left_frame, text=f"Select a measurement:")
     which_measure_dropdown_label.grid(row=2, column=0, padx=5, pady=5, sticky='e')
@@ -142,9 +157,11 @@ def main():
     # build the which measure dropdown
     which_measure_dropdown = ttk.Combobox(top_left_frame, values=list(vessel_data_json.keys()), state='readonly')
     which_measure_dropdown.set(list(vessel_data_json.keys())[0])
-    # which_measure_dropdown.grid(row=2, column=0, padx=10, pady=10)
     which_measure_dropdown.grid(row=2, column=1, padx=5, pady=5)
 
+    '''
+    Create the portion of the GUI for the time point selection
+    '''
     # Create a label for the time point selection checkboxes
     time_point_checkboxes_label = ttk.Label(top_right_frame, text=f"Select time points:")
     time_point_checkboxes_label.grid(row=1, column=0, padx=5, pady=5, sticky='e')
@@ -154,6 +171,9 @@ def main():
     time_point_checkboxes_frame.grid(row=1, column=1)
     time_point_checkboxes = {}
 
+    '''
+    Update the time point portion of the GUI based on patient and side selection
+    '''
     # dynamically update the time points when the patient ID is selected
     def update_time_points(*args):
         patient_id = patient_id_dropdown.get()
@@ -180,6 +200,9 @@ def main():
     # when the patient ID is selected, update the time points
     # patient_id_dropdown.bind('<<ComboboxSelected>>', update_time_points)
 
+    '''
+    Create the portions of the GUI for the side selection
+    '''
     side_selection_frame = ttk.Frame(top_right_frame)
     side_selection_frame.grid(row=0, column=1)
     # Create a label for the side selection radiobuttons
@@ -195,6 +218,9 @@ def main():
 
     # side_var.trace_add('write', update_time_points)
 
+    '''
+    Plot the filtered data for the selected patient ID, side, and time points
+    '''
     # 绘图区域
     fig, ax = plt.subplots(figsize=(5, 3))
     canvas = FigureCanvasTkAgg(fig, master=root)
@@ -218,6 +244,7 @@ def main():
                                     (data_loc['side'] == side)]
             sorted_data = filtered_data.sort_values(by='loc')
 
+            # A scatterplot and a line plot are created for each time point so that the points indicate actual values, but lines link the points for clarity
             ax.scatter(sorted_data['loc'], sorted_data[which_measure], color=color_map(i))
             ax.plot(sorted_data['loc'], sorted_data[which_measure], label=f'Time Point: {str(tp)}', color=color_map(i))
 
@@ -230,6 +257,15 @@ def main():
             avg_values_to_plot = average_value.reindex(sorted_data['loc'])
             ax.plot(sorted_data['loc'], avg_values_to_plot, linestyle='--', color='grey')
 
+        # It would be better to move the average calculation out of the loop. For some reason it relies on the filtered_data in the loop though...
+
+        # # Calculate and plot the average value for all data at the current time point and side
+        # # Here is little redundant, but it is easy to implement
+        # all_data = data_loc[(data_loc['tp'] == int(0)) & (data_loc['side'] == side)]
+        # average_value = all_data.groupby('loc')[which_measure].mean()
+
+        # avg_values_to_plot = average_value.reindex(sorted_data['loc'])
+        # ax.plot(sorted_data['loc'], avg_values_to_plot, linestyle='--', color='grey')
 
         ax.set_xlabel('Slice Number')
         ax.set_ylabel(which_measure_dropdown.get())
@@ -238,6 +274,9 @@ def main():
         fig.tight_layout()
         canvas.draw()
 
+    '''
+    Update the time points and plot the data when the patient ID is selected
+    '''
     def on_patient_id_selected(event=None):
         # 先更新时间点
         update_time_points()
@@ -251,10 +290,16 @@ def main():
     plot_data()
     # patient_id_dropdown.bind('<<ComboboxSelected>>', plot_data)
     
+    '''
+    Bind the data
+    '''
     patient_id_dropdown.bind('<<ComboboxSelected>>', on_patient_id_selected)
     which_measure_dropdown.bind('<<ComboboxSelected>>', plot_data)
     side_var.trace_add('write', plot_data)
 
+    '''
+    Create a popup window to display patient information
+    '''
     popup = None
 
     def Popinfo():
@@ -305,6 +350,9 @@ def main():
     open_popup_button = tk.Button(root, text="Patient Information", command=Popinfo)
     open_popup_button.grid(row=3, column=0, columnspan=2, pady=10)  # Adjusted placement for button
 
+    '''
+    Run the main loop
+    '''
     root.mainloop()
 
 
